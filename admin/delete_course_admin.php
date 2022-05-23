35b55b31c8a494ed55a7fe3acddd75cc3c3836c4
eIonut <enacheionut2399@gmail.com>
2022-05-23 08:58:32 +0300

    Added sql functionality for coruse page.

--- admin/delete_course_admin.php
+++ admin/delete_course_admin.php
@@ -14,7 +14,7 @@ include 'includes.php';
     
 <?php
 include('../dbconnection.php');
- error_reporting(0);
+//  error_reporting(0);
 if(isset($_POST['delete'])){
     $asd = mysqli_real_escape_string($con, $_POST['lesson_to_delete']);
     $id_to_delete = mysqli_real_escape_string($con, $_POST['id_to_delete']);
@@ -48,24 +48,24 @@ if (isset($_GET['course_name'])) {
     $id = mysqli_real_escape_string($con, $_GET['course_name']);
     
     //make sql
-    $sql = "SELECT * FROM courses WHERE course_name = '$id'";
-    
-    //get the query result
+    $sql = "SELECT lessons.id, lessons.name, assets.url, lessons_assets.lesson_id FROM lessons
+    INNER JOIN lessons_assets ON lessons_assets.lesson_id = lessons.id
+    INNER JOIN assets ON assets.id = lessons_assets.asset_id
+    WHERE lessons.course_id = 14
+    ORDER BY lessons.id;
+    ";
     $result = mysqli_query($con, $sql);
-    
-    //fetch result in array format
-    $course = mysqli_fetch_assoc($result);
-    
-    mysqli_free_result($result);
+
     
     
 }
 
 $asd = $_GET["course_name"];
-$comm = $_POST['comment'];
 
 if(isset($comm)){
     date_default_timezone_set('Europe/Bucharest');
+$comm = $_POST['comment'];
+
     $asdf = $_SESSION['login_admin'];
     $timestamp = date("Y-m-d H:i:s");
     $sql3 = "INSERT INTO comments(user, date, comment_content, course_name)
@@ -106,225 +106,30 @@ if(isset($_POST['update'])) {
     <title>dETAILS</title>
 </head>
 <body style="background: white;">
-<div class="container-grid">
-    <div class="sidebar text-light px-3">
-        <div class="text-light sidebar-header px-2 pt-4 mx-auto d-flex justify-content-between align-items-center  p0-collapse" style="width: 100%; overflow:hidden;">
-        <div>
-        <h3 class="hide-event m-0 p-0 welcome-top-sidebar">
-        Welcome back,
-        <br> <p class="font-weight-bold"><?php echo $_SESSION['login_admin'];?></p>
-      
-            </h3>
-            </div>
-            
-           
-            
-            <i class="fa-solid py-3 fa-expand collapse-btn sidebar-icons"></i>
-            <!-- fa-compress -->
-          
-            
-            </div>
-            <hr>
-        <div class="sidebar-top-section  p0-collapse d-flex py-2 flex-column px-2">
-
-            <div class="d-flex justify-content-between  align-items-center">
-                <a class="sidebar-links hide-event py-2" href="index_admin.php">Courses</a>
-                <i class="fa-solid fa-book-open text-center sidebar-icons"></i>
-            </div>
-            
-        </div>
-<hr>
-        <div class="add-content d-flex flex-column  p0-collapse px-2 py-2 mx-auto" style="width: 100%;">  
-        <div class="py-2 d-flex justify-content-between align-items-center">
-            <a class="sidebar-links hide-event py-2" href="add_course_admin.php">Add course</a>
-            <i class="fa-solid fa-plus text-center sidebar-icons"></i>
-        </div> 
-            
-        <div class="py-2 d-flex justify-content-between  p0-collapse  py-2 align-items-center">
-            <a class="sidebar-links hide-event py-2" href="add_class.php">Add / remove subject</a>
-            <i class="fa-solid fa-plus text-center sidebar-icons"></i>
-        </div>
-        </div>
-        <hr>
- 
-        <form action="delete_course_admin.php" class="del-course-form  p0-collapse d-flex px-2 py-2 align-items-center my-0" method="POST">
-           
-                <input  class="form-control" type="hidden" name="id_to_delete" value="<?php echo $course['course_id'];?>"> 
-                <input  class="form-control" type="hidden" name="lesson_to_delete" value="<?php echo $course['course_name'];?>"> 
-                <input  class="delete-course-btn hide-event sidebar-links" type="submit" name="delete" value="Delete course">
-                
-           
-            <i class="fa-solid fa-trash ml-auto sidebar-icons"></i> 
-        </form>
-        <hr>
-        <div class="logout-div px-2 d-flex p0-collapse flex-row py-3 justify-content-between align-items-center">
-        
-        <a
-                href="logout_admin.php"
-                id="logout-btn"
-                class="logout-btn hide-event sidebar-links px-0  mx-auto d-flex justify-content-start" style="width: 100%;"
-                >Logout
-              </a>
-              
-
-              <i class="fa-solid fa-right-from-bracket sidebar-icons"></i>
-
-              
-              </div>
-              <button class="btn my-3 text-light edit-mode-btn  d-flex align-items-center justify-content-center
-              text-center w-50 mx-auto" 
-              style="#305397; opacity: 1;
-              box-shadow: rgba(255, 255, 255, 0.5) 0px 0px 0px 2px;
-               border: none; outline: none;">Edit mode</button>
-             
-              
-    </div>
-
-    <div class="content">
-        <div class="row col-12 h-100 p-0 m-0">
-            <div class="col-xl-8 p-0 m-0">
-                <div id="videoDiv"class="videos d-flex flex-column text-center justify-content-center" style="height: 400px">
-                    <p class="mt-50">No videos to watch. Talk to an admin to add more.</p>
-                </div>
-
-                <div class="comments mt-auto p-4" style="max-height: fit-content; overflow: scroll; overflow-x:hidden;">
-                    <h4 class="border-bottom py-4 pt-0">Comments</h4>
-                        <?php
-                        $sql2 = "SELECT * from comments WHERE course_name='$asd'";
-                        $result2 = mysqli_query($con, $sql2);
-
-                        if (mysqli_num_rows($result2) > 0) {
-                            while ($row = mysqli_fetch_assoc($result2)) {
-                                
-                                echo '<div class="border-bottom py-3">';
-                                echo '<div class="d-flex">';
-                                    echo '<p class="align-self-start font-weight-bold" style="color: #305397">'.$row["user"].'</p>';
-                                    echo '<p class="align-self-end ml-auto font-weight-normal" style="opacity: 0.5">'.$row["date"].'</p>';
-                                echo '</div>';
-                                echo '<p class="font-weight-bold">'.$row["comment_content"].'</p>';
-                                // if($row['user'] == $_SESSION['login_admin']){ //pt partea
-                                echo '<div class="w-100 text-right">';
-                                echo '<a class="px-3 text-dark edit-btn" style="opacity: 0.75;" href="editcomment.php?id= '. $row['comment_id'] .';"><i class="fa-solid fa-pencil"></i></a>';
-
-                                echo '<a class="text-danger edit-btn del-btn" style="opacity: 0.75;" href="delcomment.php?id= '. $row['comment_id'] .';"><i class="fa-solid fa-trash"></i></a>';
-                                echo '</div>';
-                                // }
-                                
-                                echo '</div>';
-                            }
-                        }
-
-                        else{
-                            echo 'No comments found.';
-                        }
-                        ?>
-
-<div class="leave-comment" style="height: fit-content;">
-
-                    <form name="form" method="POST" class="was-validated" >
-                        <div class="form-group">
-                            <h4 class="my-4 p-0" >Leave a comment</h4>
-                            <textarea class="form-control is-invalid" name="comment" id="comment" placeholder="Write here your comment..." required></textarea>
-                            <div class="invalid-feedback">
-                                Please enter a message in the textarea above to write a comment.
-                            </div>
-                    
-                            <button class="btn btn-primary w-100 text-start border-0 py-2 pl-4 my-3 d-flex justify-content-between align-items-center"
-                            style="background: linear-gradient(84.57deg, #1b3d7d 0%, #4a6db0 100%);"
-                            >Send the comment<i class="fa-solid fa-xs ml-auto pr-2 fa-arrow-right"></i></button>
-                            </div>
-                    </form>
-                </div>
-                </div>
-
-                
-            </div>
-        
-
-        
-            
-            <div class="col-xl-4 border-info p-0 m-0 courses-div" style="background: #eaeef5; height:max-height: 120vh;">
-            <h4 class="mx-3 py-4" style="color: #305397;"><?php
-    echo htmlspecialchars($course['course_name']);
-?></h4>
-            <?php
-             
-             $sql    = "SELECT * FROM lessons WHERE lesson_subject= '$asd' ORDER BY lesson_order ASC";
-             $result = mysqli_query($con, $sql);
-             $result_array = mysqli_fetch_array($result);
-
-             
-             
-           
-             if (mysqli_num_rows($result) > 0) {
-              
-                 while ($row = mysqli_fetch_assoc($result)) {
-                    if (str_contains($row["pdf_location"], '.mp4')) {
-                    
-            echo '<button class="accordion py-4 font-weight-bold mx-4 d-flex justify-content-between align-items-center" style="background: #eaeef5;">' . $row['lesson_order'] . '.' . " " . $row["lesson_number"] .'
-            <div class="ml-auto pr-3">
-            <a class="px-3 text-dark edit-btn" style="opacity: 0.75;" href="edit_lesson.php?id= '. $row['id'] .';"><i class="fa-solid fa-file-pen"></i></a>
-            <a class="text-danger edit-btn" style="opacity: 0.75;" href="delete_lesson.php?id= '. $row['id'] .';"><i class="fa-solid fa-trash"></i></a>
-            </div>
-            </button>';
-            echo '<div class="panel" style="background: #eaeef5;">';
-            echo '<video class="course-video" src="../images/' . $row["pdf_location"] . '" width="100%" height="300px" controls volume="1">';
-
-            echo ' </video>';
-            
-          
-                
-          
-            echo '<div class="mb-4">';
-            echo '<i class="fa-solid fa-stopwatch mr-2 text-start"></i>';
-            echo '<p class="timer bolded mr-auto d-inline" style="opacity: 0.75;">Duration: <span class="video-duration"></span></p>';
-            echo '</div>';
-            echo '<button class="play-btn btn btn-primary w-100 text-start border-0 py-2 pl-4 d-flex justify-content-between align-items-center" 
-            style="background: linear-gradient(84.57deg, #1b3d7d 0%, #4a6db0 100%);" type="button">See this lesson<i class="fa-solid fa-xs ml-auto pr-2 fa-arrow-right"></i></button>';
-           
-            echo '</div>'; 
-            
-                    }
-                    else{
-                         
-        echo '<button class="accordion py-4 mx-4 font-weight-bold d-flex justify-content-between align-items-center" style="background: #eaeef5;">' . $row['lesson_order'] . '.' . " " . $row["lesson_number"] . " - (Resources)".'
-        <div class="ml-auto pr-3">
-        <a class="px-3 text-dark edit-btn" style="opacity: 0.75;" href="edit_lesson.php?id= '. $row['id'] .';"><i class="fa-solid fa-file-pen"></i></a>
-        <a class="text-danger edit-btn  del-btn" style="opacity: 0.75;" href="delete_lesson.php?id= '. $row['id'] .';"><i class="fa-solid fa-trash"></i></a>
-        </div>
-        </button>';
-         ;
-         
-        echo '<div class="panel" style="background: #eaeef5;"> ';
-        echo '<p>';
-        echo '<span class="bolded bolded-file font-weight-bold pr-1" style="font-size: 0.9em;">File: </span>' . '<span class="text-underline" style="font-size: 0.9em; color: #305397;" ><u>'.$row["pdf_location"].'</u></span>';
-        echo '</p>';
-        echo "<br>";
-        
-        echo '<div class="d-flex justify-content-between align-items-center">';
-        echo '<a class="mr-2" style="opacity: 0.75; color: #305397;" href="../images/' . $row["pdf_location"] . '" target="_blank">Download File </a>';
-        echo '<i class="fas fa-md fa-file-download mr-auto" style="opacity: 0.75; color: #305397;"></i>';
-        echo '</div>';
-      
-
-       
-        echo '</div>';
-       
-                    }
-                }
-            }
-                    else {
-                        echo '<div class="no-subjects px-3">';
-                         echo "<p>No subjects were added to this course so far. Talk to an administrator for adding.</p>";
-                        echo "</div>";
-                    }
-            ?>    
-            </div>
-        </div>
-    </div>
-</div>
+<div class="col-xl-4 border-info p-0 m-0 courses-div" style="background: #eaeef5; height:max-height: 120vh;">
+<?php
+$collecting_names = array();
+$contor = 0;
+while($row = mysqli_fetch_assoc($result)){
   
+    $collecting_names[] = $row['name'];
+    $collecting_names = array_unique($collecting_names);
+    if($row['lesson_id'] == $row['id']){
+        if($contor < count($collecting_names)){
+echo '<button class="accordion py-4 font-weight-bold mx-4 d-flex justify-content-between align-items-center" style="background: #eaeef5;">' . $collecting_names[$contor] . '</button>';
+
+        $contor ++;
+        }  
+        
+        echo '<p>'.$row['url'].'</p>';
+        echo '<a class="mr-2" style="opacity: 0.75; color: #305397;" href="../images/' . $row["url"] . '" target="_blank">Download File </a>';
+    }
+  }
+?>
+</div>
+
 </body>
+
 
 
 <script>
@@ -334,5 +139,5 @@ if ( window.history.replaceState ) {
 }
 
 </script>
-<script src="js/newAccordionV2.js?v=dasgdfgdadassdasadaddaadassdadaDAddadadsadasdaadadadadadadadadaddadaadadadaddsSafasdafssfsdadadaadadadaassddasAddaasdsagsf"></script>
+<script src="js/newAccordionV2.js?v=dasgdfgdadassdasadaddaadassdadaDAdaddadadsadadasdaadadadadadadadadaddadaadadadaddsSafasdafssfsdadadaadadadaassddasAddaasdsagsf"></script>
 </html>
--- admin/js/newAccordionV2.js
+++ admin/js/newAccordionV2.js
@@ -1,34 +1,34 @@
-const collapseBtn = document.querySelector(".collapse-btn");
-const gridContainer = document.querySelector(".container-grid");
-const sidebar = document.querySelector(".sidebar");
-const editBtns = document.querySelectorAll(".edit-btn");
-const editModeToggle = document.querySelector(".edit-mode-btn");
-const editIcon = document.querySelector(".edit-icon");
+// // const collapseBtn = document.querySelector(".collapse-btn");
+// // const gridContainer = document.querySelector(".container-grid");
+// // const sidebar = document.querySelector(".sidebar");
+// // const editBtns = document.querySelectorAll(".edit-btn");
+// // const editModeToggle = document.querySelector(".edit-mode-btn");
+// // const editIcon = document.querySelector(".edit-icon");
 
-collapseBtn.addEventListener("click", function () {
-  let collapseDivsPadding = document.getElementsByClassName("p0-collapse");
-  for (var i = 0; i < collapseDivsPadding.length; i++) {
-    collapseDivsPadding[i].classList.toggle("p0-custom");
-  }
+// // collapseBtn.addEventListener("click", function () {
+// //   let collapseDivsPadding = document.getElementsByClassName("p0-collapse");
+// //   for (var i = 0; i < collapseDivsPadding.length; i++) {
+// //     collapseDivsPadding[i].classList.toggle("p0-custom");
+// //   }
 
-  let icons = document.getElementsByClassName("sidebar-icons");
-  for (var i = 0; i < icons.length; i++) {
-    icons[i].classList.toggle("mx-auto");
-    icons[i].classList.toggle("opacity-100");
-  }
-  gridContainer.classList.toggle("collapsed");
-  var els = document.getElementsByClassName("hide-event");
-  for (var i = 0; i < els.length; i++) {
-    els[i].classList.toggle("d-none");
-    els[i].classList.toggle("mx-auto");
-  }
+// //   let icons = document.getElementsByClassName("sidebar-icons");
+// //   for (var i = 0; i < icons.length; i++) {
+// //     icons[i].classList.toggle("mx-auto");
+// //     icons[i].classList.toggle("opacity-100");
+// //   }
+// //   gridContainer.classList.toggle("collapsed");
+// //   var els = document.getElementsByClassName("hide-event");
+// //   for (var i = 0; i < els.length; i++) {
+// //     els[i].classList.toggle("d-none");
+// //     els[i].classList.toggle("mx-auto");
+// //   }
 
-  if (gridContainer.classList.contains("collapsed")) {
-    editModeToggle.innerHTML = `<i class="fa-solid mx-auto fa-wand-magic "></i>`;
-  } else {
-    editModeToggle.textContent = "Edit Mode";
-  }
-});
+// //   if (gridContainer.classList.contains("collapsed")) {
+// //     editModeToggle.innerHTML = `<i class="fa-solid mx-auto fa-wand-magic "></i>`;
+// //   } else {
+// //     editModeToggle.textContent = "Edit Mode";
+// //   }
+// // });
 
 var acc = document.getElementsByClassName("accordion");
 var panels = document.getElementsByClassName("panel");
@@ -52,57 +52,58 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-playBtn.forEach((item, index) => {
-  item.addEventListener("click", (event) => {
-    videosDiv.innerHTML = ``;
-    let source = courseVideo[index].currentSrc;
-    videosDiv.innerHTML = `<video
-    class="course-video" src=${source} width="100%" height="100%" controls volume="1">';
-    </video>`;
-  });
-});
+// // }
+// // playBtn.forEach((item, index) => {
+// //   item.addEventListener("click", (event) => {
+// //     videosDiv.innerHTML = ``;
+// //     let source = courseVideo[index].currentSrc;
+// //     videosDiv.innerHTML = `<video
+// //     class="course-video" src=${source} width="100%" height="100%" controls volume="1">';
+// //     </video>`;
+// //   });
+// // });
 
-window.addEventListener("DOMContentLoaded", function () {
-  editBtns.forEach((item) => {
-    item.classList.add("d-none");
-  });
-  courseVideo.forEach((item, index) => {
-    item.style.display = "none";
-    item.addEventListener("loadedmetadata", (event) => {
-      var duration = item.duration;
-      var minutes = parseInt(duration / 60, 10);
-      var seconds = duration % 60;
-      videoDuration[index].textContent = `${minutes}min ${seconds.toFixed(0)}s`;
-    });
-  });
+// // window.addEventListener("DOMContentLoaded", function () {
+// //   editBtns.forEach((item) => {
+// //     item.classList.add("d-none");
+// //   });
+// //   courseVideo.forEach((item, index) => {
+// //     item.style.display = "none";
+// //     item.addEventListener("loadedmetadata", (event) => {
+// //       var duration = item.duration;
+// //       var minutes = parseInt(duration / 60, 10);
+// //       var seconds = duration % 60;
+// //       videoDuration[index].textContent = `${minutes}min ${seconds.toFixed(0)}s`;
+// //     });
+// //   });
 
-  courseVideo.forEach((items) => {
-    panels.forEach((item) => {
-      if (item.contains(items)) {
-        videosDiv.innerHTML = `<video
-        class="course-video" src=${courseVideo[0].currentSrc} width="100%" height="100%" controls volume="1">';
-        </video>`;
-      }
-    });
-  });
-});
+// //   courseVideo.forEach((items) => {
+// //     panels.forEach((item) => {
+// //       if (item.contains(items)) {
+// //         videosDiv.innerHTML = `<video
+// //         class="course-video" src=${courseVideo[0].currentSrc} width="100%" height="100%" controls volume="1">';
+// //         </video>`;
+// //       }
+// //     });
+// //   });
+// // });
 
-const convertVideoDuration = (video) => {
-  var minutes = parseInt(video.duration / 60, 10);
-  var seconds = video.duration % 60;
-  const videoTimer = [];
-  videoTimer.push(minutes, seconds);
-  return videoTimer;
-};
+// // const convertVideoDuration = (video) => {
+// //   var minutes = parseInt(video.duration / 60, 10);
+// //   var seconds = video.duration % 60;
+// //   const videoTimer = [];
+// //   videoTimer.push(minutes, seconds);
+// //   return videoTimer;
+// // };
 
-editModeToggle.addEventListener("click", () => {
-  if (editModeToggle.style.opacity == "0.75") {
-    editModeToggle.style.opacity = "1";
-  } else {
-    editModeToggle.style.opacity = "0.75";
-  }
+// // editModeToggle.addEventListener("click", () => {
+// //   if (editModeToggle.style.opacity == "0.75") {
+// //     editModeToggle.style.opacity = "1";
+// //   } else {
+// //     editModeToggle.style.opacity = "0.75";
+// //   }
 
-  editBtns.forEach((item) => {
-    item.classList.toggle("d-none");
-  });
-});
+// //   editBtns.forEach((item) => {
+// //     item.classList.toggle("d-none");
+// //   });
+// // });
